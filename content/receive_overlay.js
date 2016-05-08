ea78f35c36cbb4d284c2e5b8f369e5e208510158
Greg Rehm <grehm87@gmail.com>
2016-05-08 13:24:05 -0700

    i did it! this is a thing!

--- content/receive_overlay.js
+++ content/receive_overlay.js
@@ -4,38 +4,26 @@ Cu.import("chrome://megthunderbird/content/db.js");
 Cu.import("chrome://megthunderbird/content/crypto.js");
 Cu.import("chrome://megthunderbird/content/http.js");
 
-//let times = 0;
 let ss = new Storage("megthunderbird");
 let crypto = new Crypto(ss);
+let messenger = Components.classes["@mozilla.org/messenger;1"]
+    .createInstance(Components.interfaces.nsIMessenger);
+let listener = Components.classes["@mozilla.org/network/sync-stream-listener;1"]
+    .createInstance(Components.interfaces.nsISyncStreamListener);
+let ioService = Components.classes["@mozilla.org/network/io-service;1"]
+    .getService(Components.interfaces.nsIIOService);
 
 decryptorCallback = function(text) {
     var message = JSON.parse(text).message;
     var plain = crypto.decryptText(message);
-    Cu.reportError(plain);
+    var messagepane = document.getElementById("messagepane").contentWindow.document;
+    var textFlowed = messagepane.getElementsByClassName("moz-text-flowed");
+    textFlowed[0].text = plain;
 }
 
 var messageExtension = {
 	init: function() {
-        // Use this or the one below??
-		var messagePane = GetMessagePane();
-//        for (var m in messagePane) {
-//            Cu.reportError("message pane " + m);
-//        }
-//        Cu.reportError(messagePane.TEXT_NODE);
-//        Cu.reportError(messagePane.textContext);
-//        Cu.reportError(messagePane.children);
-//        for (var i=0; i < messagePane.children.length; i++){
-//            Cu.reportError(typeof messagePane.children[i]);
-//            Cu.reportError(messagePane.children[i].constructor.name);
-//            Cu.reportError(messagePane.children[i].nodeName);
-//        }
 		var messagepane = document.getElementById("messagepane"); // mail
-//        for (var i=0; i < messagepane.children.length; i++){
-//            Cu.reportError(messagePane.children[i].nodeName);
-//        }
-        //for (var m in messagepane) {
-        //    Cu.reportError("message pane " + m);
-        //}
 		if(messagepane){
 			messagepane.addEventListener("load", function(event) {
 				messageExtension.onPageLoad(event);
@@ -45,22 +33,9 @@ var messageExtension = {
 	onPageLoad: function(aEvent) {
         var doc = aEvent.originalTarget; // doc is document that triggered the event
         var win = doc.defaultView; // win is the window for the doc
-        //for (var m in doc) {
-        //    Cu.reportError("doc " + m);
-        //}
-        // test desired conditions and do something
-        // if (doc.nodeName != "#document") return; // only documents
-        // if (win != win.top) return; //only top window.
-        // if (win.frameElement) return; // skip iframes/frames
-        Cu.reportError("page is loaded \n" +doc.location.href);
-		let messenger = Components.classes["@mozilla.org/messenger;1"]
-								  .createInstance(Components.interfaces.nsIMessenger);
-		let listener = Components.classes["@mozilla.org/network/sync-stream-listener;1"]
-							   .createInstance(Components.interfaces.nsISyncStreamListener);
-		var ioService = Components.classes["@mozilla.org/network/io-service;1"]
-								  .getService(Components.interfaces.nsIIOService);
 		var url = ioService.newURI(doc.location.href, null, null);
         // Is of type xpconnect wrapped nsIMsgDBHeader
+        // But if you want to look up the actual object it is msIMsgDBHdr
 		msgHdr = url.QueryInterface(Components.interfaces.nsIMsgMessageUrl).messageHeader;
         // TODO Eventually we will want to declare false here to not strip html
         var text = msgHdrToMessageBody(msgHdr, true, 32768);
@@ -73,19 +48,6 @@ var messageExtension = {
         http.getDecryptedFromServer(
             decryptorCallback, function() {}, recipient, author
         );
-        // I don't think this will work for us. It deletes the message and thunderbird
-        // isn't able to keep track of the changes after deletion. You need to restart.
-        // I think we can try to figure out how to put the new messge text on top of the
-        // old. It's html after all. I don't think it'll be impossible.
-//        if (times < 1) {
-//            msgHdrsModifyRaw([msgHdr], function(input) {
-//                //We can't find the whole text because input includes newlines
-//                var idx = input.search(text.slice(0,10));
-//                Cu.reportError(input.slice(0, idx));
-//                return input.slice(0, idx) + "\nblah blah blah blah blah";
-//            });
-//            times += 1;
-//        }
     }
 }
 
